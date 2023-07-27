ec7d2e6c68772d15786f4dc4be3372a681865513
princewritecode <princewritecode@gmail.com>
2023-07-27 19:44:00 +0530

    loops and conditions in EJS

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 /* Now let's understand how ejs works that is embedded javascript */
-
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = 80;
-
+const redditData = require("./data.json");
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
-
 app.get('/cats', (req, res) => {
     const cats = ['Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'];
     res.render('cats.ejs', { cats });
 });
-
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
     res.render('random.ejs', { rand: num });
 });
 app.get("/r/:subreddit", (req, res) => {
     const { subreddit } = req.params;
-    res.render('subreddit.ejs', { subreddit });
+    const data = redditData[subreddit];
+    console.log(data);
+    res.render('subreddit.ejs', { ...data });
 });
-
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.listen(port, () => {
